cfd6ee262449133b9acbb7e4f1ed815354d37b80
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-03-21 17:19:26 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,37 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static void selectionSortName(String[] names, int[] ages) {
+		for (int i = 0; i < names.length - 1; i++) {
+			int currLowest = i;
+			for (int e = i; e < names.length; e++) {
+				if (names[e].compareToIgnoreCase(names[currLowest]) < 0) {
+					currLowest = e;
+				}
+			}
+			String tempName = names[currLowest];
+			names[currLowest] = names[i];
+			names[i] = tempName;
+			int tempAge = ages[currLowest];
+			ages[currLowest] = ages[i];
+			ages[i] = tempAge;
+		}
+	} 
 
+	public static void selectionSortAge(String[] names, int[] ages) {
+		for (int i = 0; i < ages.length - 1; i++) {
+			int currLowest = i;
+			for (int e = i; e < ages.length; e++) {
+				if (ages[e] < ages[currLowest]) {
+					currLowest = e;
+				}
+			}
+			String tempName = names[currLowest];
+			names[currLowest] = names[i];
+			names[i] = tempName;
+			int tempAge = ages[currLowest];
+			ages[currLowest] = ages[i];
+			ages[i] = tempAge;
+		}
+	}
 }
